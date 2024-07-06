ae810d8825b712479f8e37a40891d49ebb42323e
cheived <cheived55@gmail.com>
2024-07-06 10:35:56 +0400

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import '@telegram-apps/telegram-ui/dist/styles.css';
 
 import { AppRoot, Input, List, Placeholder, Select, Tappable } from '@telegram-apps/telegram-ui';
+import { useEffect, useState } from 'react';
+import WebApp from '@twa-dev/sdk'
 
 function App() {
+
+  const [amount, setAmount] = useState('');
+  const [comment, setComment] = useState('');
+
+  useEffect(() => {
+    if (amount.length > 0 && comment.length > 0) {
+      console.log("event")
+      WebApp.sendData(JSON.stringify({ amount, comment }))
+    }
+  }, [amount, comment])
+
   return (
     <>
-      <Select header="Category">
+      {/* <Select header="Category">
         <option>Select category...</option>
         <option>Hello</option>
         <option>Okay</option>
-      </Select>
-      <Input placeholder='Type amount...' header='Amount' />
-      <Input placeholder='Type comment' header='Comment' />
+      </Select> */}
+      <Input placeholder='Type amount...' header='Amount' onChange={(event) => setAmount(event.target.value)} />
+      <Input placeholder='Type comment' header='Comment' onChange={(event) => setComment(event.target.value)} />
     </>)
 }
 
